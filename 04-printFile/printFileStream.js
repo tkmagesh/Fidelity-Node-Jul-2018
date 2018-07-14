2ad1823017a34630f2186c367c13358340514029
Magesh <tkmagesh77@gmail.com>
2018-07-14 16:05:26 +0530

    file read operations - complete

---
+++ 04-printFile/printFileStream.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream('sample.txt', {encoding :'utf8'});
+
+stream.on('open', function(){
+	console.log('file opened for reading');
+});
+stream.on('data', function(chunk){
+	console.log(chunk);
+	//console.log('data event triggered');
+});
+stream.on('end', function(){
+	console.log('end of stream reached');
+});
+stream.on('close', function(){
+	console.log('file closed');
+});
+
+stream.on('error', function(err){
+	console.log('something went wrong ', err);
+});
